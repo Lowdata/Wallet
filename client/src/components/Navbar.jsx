63ba3894bdef801652c5d9aa2ae9109f5b128b3d
ayush.pahuja <ayush.pahuja090@gmail.com>
2022-06-10 13:29:52 +0530

    error fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,28 @@
 import {HiMenuAlt4} from 'react-icons/hi';
 import {AiOutlineClose} from 'react-icons/ai';
 import logo from '../../images/logo.png';
+const NavbarItem = ((title, classProps)) => {
+    return(
+        <li className={'mx-4 cusor-pointer ${classProps}'}>
+            {title }
+        </li>
+    )
+
+} 
+    
 
 const Navbar = ( ) => {
+    
     return(
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src='client\images\logo.png' alt='logo' className='w-32 cursor-pointer' />
-                '
+                <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+                    <NavbarItem />
+
+                </ul>
+
+                
             </div>
         </nav>
     );
