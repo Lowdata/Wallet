5db142325f487b5a72b11b97c164258f6be7bb99
ayush.pahuja <ayush.pahuja090@gmail.com>
2022-06-10 21:11:16 +0530

    login

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,12 @@ const Navbar = ( ) => {
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
                 <img src='client\images\logo.png' alt='logo' className='w-32 cursor-pointer' />
                 <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-                    <NavbarItem />
+                    {["Market", "Tutorials", "Wallets"].map((item, index )=> (
+                        <NavbarItem key={item+index} title={item}/>
+                    ))}
+                    <li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[2546bd] '>
+                        login
+                    </li>
 
                 </ul>
 
